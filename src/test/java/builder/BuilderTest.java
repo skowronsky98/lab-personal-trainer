952b4e3e5ba1c15d634e85e3f47f57b02a8b0cf1
skowronsky <oskar.programming@gmail.com>
2021-01-07 14:40:33 +0100

    builder tests

--- src/test/java/builder/BuilderTest.java
+++ src/test/java/builder/BuilderTest.java
@@ -10,54 +10,42 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuilderTest {
+    
+    @Test
+    void createStandardPlan(){
 
-    ArrayList<WeekPlan> weekPlans = new ArrayList<>(Arrays.asList(new WeekPlan(),new WeekPlan()));
-    ArrayList<DailyPlan> dailyPlans = new ArrayList<>(Arrays.asList(new DailyPlan()));
-    ArrayList<MealPlan> mealPlans = new ArrayList<>(Arrays.asList(new MealPlan()));
+        Director director = new Director();
 
-//    @Test
-//    void createPremiumPlan(){
-//
-//        PlanStandardBuilder planStandardBuilder = new PlanStandardBuilder();
-//        planStandardBuilder.setMealPlan(mealPlans);
-//        planStandardBuilder.setDailyPlan(dailyPlans);
-//        planStandardBuilder.setWeekPlan(weekPlans);
-//        planStandardBuilder.setPlanType();
-//
-//        Plan plan = planStandardBuilder.build();
-//
-//        assertTrue(plan instanceof PlanStandardBuilder);
-//    }
-//    @Test
-//    void comparePlan(){
-//
-//        Plan expectedPlan = new Plan(weekPlans,dailyPlans,mealPlans,planType);
-//
-//        PlanStandardBuilder planStandardBuilder = new PlanStandardBuilder();
-//        planStandardBuilder.setPlanType();
-//
-//        Plan plan = planStandardBuilder.build();
-//
-//        assertEquals(expectedPlan.getPlanType(),plan.getPlanType());
-//
-//    }
-//
-//    @Test
-//    void valueNotSet(){
-//        PlanStandardBuilder planStandardBuilder = new PlanStandardBuilder();
-//
-//        Plan plan = planStandardBuilder.build();
-//
-//        assertEquals(plan.getMealPlans(),null);
-//    }
-//
-//    @Test
-//    void instanceTest(){
-//        PlanStandardBuilder planStandardBuilder = new PlanStandardBuilder();
-//
-//        Plan plan = planStandardBuilder.build();
-//
-//        assertEquals(plan.getClass(),Plan.class);
-//    }
+        Builder planStandardBuilder = new PlanStandardBuilder();
+        Plan plan = director.constructStandardPlan(planStandardBuilder);
+
+        assertEquals(PlanType.STANDARD,plan.getPlanType());
+    }
+    @Test
+    void createPremiumPlan(){
+
+        Director director = new Director();
+
+        Builder planStandardBuilder = new PlanStandardBuilder();
+        Plan standardPlan = director.constructStandardPlan(planStandardBuilder);
+
+        Builder planPremiumBuilder = new PlanPremiumBuilder();
+        Plan premiumPlan = director.constructPremiumPlan(planPremiumBuilder);
+
+
+        assertNotEquals(standardPlan.getPlanType(),premiumPlan.getPlanType());
+    }
+
+    @Test
+    void instancePremiumPlan(){
+
+        Director director = new Director();
+
+        Builder planPremiumBuilder = new PlanPremiumBuilder();
+        Plan premiumPlan = director.constructPremiumPlan(planPremiumBuilder);
+
+        assertTrue(premiumPlan instanceof Plan);
+
+    }
 
 }
